a5216a2c3df69fd49600b4a106ea7b4dc3908323
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2024-11-26 17:37:33 +0200

    fix: Attempt to add a field EWX-669.

--- en.dict.eworx-pc-class-extra-fields.php
+++ en.dict.eworx-pc-class-extra-fields.php
@@ -8,5 +8,11 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:PC/Attribute:bicycle' => 'Bicycle',
+	'Class:PC/Attribute:bicycle+' => 'The brand of your bicycle',
+	'Class:PC/Attribute:wheel' => 'Wheel Size',
+	'Class:PC/Attribute:wheel+' => '',
+	'Class:PC/Attribute:stem' => 'Stem',
+	'Class:PC/Attribute:stem+' => 'Please include info about bars diameter, offset etc',
 ));
 ?>
--- module.eworx-pc-class-extra-fields.php
+++ module.eworx-pc-class-extra-fields.php
@@ -15,7 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			
+			'itop-storage-mgmt/3.2.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
