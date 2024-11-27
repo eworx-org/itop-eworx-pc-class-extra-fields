d820bf4080f1c8bcea79f4c49892694b55530119
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2024-11-27 09:35:04 +0200

    refactor: Prefix all new fields with `ewx_` to prevent collisions EWX-669.

--- en.dict.eworx-pc-class.php
+++ en.dict.eworx-pc-class.php
@@ -8,13 +8,13 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
-	'Class:PC/Attribute:bicycle' => 'Bicycle',
-	'Class:PC/Attribute:bicycle+' => 'The brand of your bicycle',
-	'Class:PC/Attribute:wheel' => 'Wheel Size',
-	'Class:PC/Attribute:wheel+' => '',
-	'Class:PC/Attribute:stem' => 'Stem',
-	'Class:PC/Attribute:stem+' => 'Please include info about bars diameter, offset etc',
-	'Class:PC/Attribute:history' => 'History (changes etc)',
-	'Class:PC/Attribute:history+' => 'Anything specific to H/W changes and maintenance to this device',
+	'Class:PC/Attribute:ewx_bicycle' => 'Bicycle',
+	'Class:PC/Attribute:ewx_bicycle+' => 'The brand of your bicycle',
+	'Class:PC/Attribute:ewx_wheel' => 'Wheel Size',
+	'Class:PC/Attribute:ewx_wheel+' => '',
+	'Class:PC/Attribute:ewx_stem' => 'Stem',
+	'Class:PC/Attribute:ewx_stem+' => 'Please include info about bars diameter, offset etc',
+	'Class:PC/Attribute:ewx_history' => 'History (changes etc)',
+	'Class:PC/Attribute:ewx_history+' => 'Anything specific to H/W changes and maintenance to this device',
 ));
 ?>
